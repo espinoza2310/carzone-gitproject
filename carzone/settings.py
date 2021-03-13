0bcca54958b7f6a2ce1ac5d9b8210123857b92a4
espinoza <espinozatest2@gmail.com>
2021-03-13 13:23:44 +0000

    Completed teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD':'310719',
+        'HOST':'localhost',
     }
 }
 
@@ -128,4 +131,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static')
 ]
 
+ # Media settings
  
